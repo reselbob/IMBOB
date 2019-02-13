ab2bacdc45d88a6eb2ee7e065288e08e50e6ca4d
Bob Reselman <reselbob@gmail.com>
2019-02-12 19:16:43 -0800

    cleaned up code and added a movie

--- data/index.js
+++ data/index.js
@@ -1,13 +1,9 @@
-const persons = require('./persons');
 const {getCollection} = require('./json/fileHelper');
-//const movies = fileToObject('movies.json');
-const movies = require('./movies');
-const {tripleHelpers} = require('./triples');
+//const {tripleHelpers} = require('./triples');
 
 
 module.exports = {
     persons: getCollection('persons'),
     movies: getCollection('movies'),
     triples: getCollection('triples') ,
-    tripleHelpers
 }
