cd054729da761fade3a579e01312f94c8c300e01
Bob Reselman <reselbob@gmail.com>
2019-02-24 12:57:00 -0800

    Modified git ignore

--- graphql/resolverHelper.js
+++
@@ -1,7 +0,0 @@
-const _ = require('lodash');
-const uuidv4 = require('uuid/v4');
-const {persons, movies, triples} = require('../data/index');
-
-const addPerson= (person) =>{
-    //make sure the movie
-}
