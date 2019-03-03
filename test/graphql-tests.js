8097a70190fbfd110a0d28ecb15688889271358c
Bob Reselman <reselbob@gmail.com>
2019-03-02 18:18:55 -0800

    adding mutation unit tests

--- test/graphql-tests.js
+++ test/graphql-tests.js
@@ -8,7 +8,8 @@ const it = require('mocha').it;
 const {persons, movies, triples} = require('../data/index');
 const server = require('../server');
 const url = 'http://localhost:4000/';
-//const request = require('supertest')(url);
+const uuidv4 = require('uuid/v4');
+var faker = require('faker');
 
 
 before(() => {
@@ -21,12 +22,20 @@ after(() => {
 
 describe('GraphQL Basic Tests', () => {
     it('Persons from API length equals Persons from data length', (done) => {
-        const query = `query { persons{ firstName lastName } }`
+        const query = `query { persons{ firstName lastName } }`;
         request(url, query)
             .then(data => {
             console.log(data);
             expect(persons.length).to.equal(data.persons.length);
             done();
         })
+    });
+
+    it('Can add person via GraphQL', (done) => {
+        const firstName = faker.name.firstName();
+        const lastName = faker.name.lastName();
+        const dob = faker.date.between('1950-01-01', '2001-12-31').toISOString().slice(0,10);
+        const query = `query { persons{ firstName lastName } }`;
+        done()
     });
 });
